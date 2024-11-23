3b2fd4ecbb4e970d7d7551242659893ff6574a43
Eashan Hatti <eashan.hatti@yale.edu>
2024-11-23 01:54:59 -0500

    Initial commit

---
+++ Elaborator.v
@@ -0,0 +1,96 @@
+Record raw_elaborator {V C : Type}
+:= {
+  fwd : V -> C;
+  bwd : C -> V;
+}.
+Arguments raw_elaborator : clear implicits.
+
+Record is_elaborator {V C : Type}
+  (VJ : V -> Prop)
+  (CJ : C -> Prop)
+  (elab : raw_elaborator V C)
+:= {
+  correct  : forall e, VJ e -> CJ (elab.(fwd) e);
+  faithful : forall e, VJ e -> VJ (elab.(bwd) (elab.(fwd) e))
+}.
+
+Inductive V := VA (l r : V) | VB (b : bool).
+Definition C := list bool.
+
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Fixpoint fwdV (v : V) : C :=
+  match v with
+  | VA l r => fwdV l ++ fwdV r
+  | VB b => [b]
+  end.
+
+Fixpoint bwdC (c : C) : V :=
+  match c with
+  | [] => VB false
+  | [b] => VB b
+  | b :: c => VA (VB b) (bwdC c)
+  end.
+
+Definition elab := {|
+  fwd := fwdV;
+  bwd := bwdC
+|}.
+
+Inductive JV : V -> Prop :=
+| JVA l r : JV l -> JV r -> JV (VA l r)
+| JVB : JV (VB true).
+
+Inductive CJ : C -> Prop :=
+| JCS : CJ [true]
+| JCC c : CJ c -> CJ (true :: c).
+
+Lemma CJ_split :
+  forall e1 e2, CJ e1 -> CJ e2 -> CJ (e1 ++ e2).
+Proof.
+  intros. induction H;
+  now constructor.
+Qed.
+
+Lemma VJ_flat :
+  forall c, CJ c -> JV (bwdC c).
+Proof.
+  intros. induction H.
+  { constructor. }
+  {
+    cbn. destruct c.
+    {
+      remember [].
+      now destruct H.
+    }
+    {
+      constructor.
+      constructor.
+      easy.
+    }
+  }
+Qed.
+
+Lemma elab_correct :
+  forall e, JV e -> CJ (fwdV e).
+Proof.
+  intros.
+  induction H.
+  { now apply CJ_split. }
+  { constructor. }
+Qed.
+
+Lemma elab_faithful :
+  forall e, JV e -> JV (bwdC (fwdV e)).
+Proof.
+  intros.
+  now apply VJ_flat, elab_correct.
+Qed.
+
+Theorem elab_is_elaborator : is_elaborator JV CJ elab.
+Proof.
+  constructor.
+  { apply elab_correct. }
+  { apply elab_faithful. }
+Qed.
